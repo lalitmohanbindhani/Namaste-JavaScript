a5cbef148fc2e8e7926559d4bfdf6d5bd81f6c78
Lalit Mohan Bindhani <bindhanilalitmohan@gmail.com>
2026-02-03 10:31:47 +0530

    function currying

--- curryingInJS_27/index.js
+++ curryingInJS_27/index.js
@@ -1,18 +1,32 @@
-let multiply = function (x, y) {
-  console.log(x * y);
-};
+// function currying can be achieved by 2 Ways
+// 1. bind function
+// 2.clouseres
 
-let multiply2 = function (x) {
+// bind method
+// we make a copy of one method and we make more methods out of it
+// by preseting some arguments inside the functios
+
+function multiply(x, y) {
+  console.log(x * y);
+}
+
+let multiplyBYTwo = multiply.bind(this, 2, 5);
+multiplyBYTwo(3);
+
+let multiplyByThree = multiply.bind(this);
+multiplyByThree(2, 3);
+
+// clouseres
+
+function multiply(x) {
   return function (y) {
     console.log(x * y);
   };
-};
+}
 
-// let multipyByTwo = multiply.bind(this, 2);
-// multipyByTwo(3);
+let result = multiply(2);
+result(3);
 
-// let multiplyByThree = multiply.bind(this, 3);
-// multiplyByThree(5);
-
-let multiplyByThree = multiply2(3);
-multiplyByThree(5);
+// bind method
+// we make a copy of one method and we make more methods out of it
+// by preseting some arguments inside the functios
